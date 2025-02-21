3a66c449f81834abdd25ab7a4fedd3a03c744140
7vanael <kristi.n.norris777@gmail.com>
2025-02-20 18:09:40 -0700

    euler 2 is in

--- spec/euler/level1/problem002_spec.clj
+++ spec/euler/level1/problem002_spec.clj
@@ -10,10 +10,40 @@
 
 ;Find the sum of the even valued terms for n<= 4,000,000
 
-(describe "Euler Problem #2"
-
-  (it "Solves #2"
-    (should= -1 (euler-2 -1)))
+  (describe "Euler Problem #2"
+      (it "finds the fibonacci numbers up to n (n>=2)"
+          (should= [1 2] (get-fibonacci-to-n 2))
+          (should= [1 2 3] (get-fibonacci-to-n 3))
+          (should= [1 2 3] (get-fibonacci-to-n 4))
+          (should= [1 2 3 5] (get-fibonacci-to-n 5))
+          (should= [1 2 3 5 8] (get-fibonacci-to-n 8))
+          (should= [1 2 3 5 8 13] (get-fibonacci-to-n 13))
+          (should= [1 2 3 5 8 13 21] (get-fibonacci-to-n 21))
+          (should= [1 2 3 5 8 13 21 34] (get-fibonacci-to-n 34))
+          (should= [1 2 3 5 8 13 21 34 55] (get-fibonacci-to-n 55))
+          (should= [1 2 3 5 8 13 21 34 55 89] (get-fibonacci-to-n 89))
+      )
+      (it "gets the sum of even numbers from a vector"
+          (should= 2 (get-sum-of-even [1 2]))
+          (should= 2 (get-sum-of-even [1 2 3]))
+          (should= 2 (get-sum-of-even [1 2 3 5]))
+          (should= 10 (get-sum-of-even [1 2 3 5 8]))
+      )
+    (it "Solves #2"
+      (should= 2 (euler-2 2))
+      (should= 2 (euler-2 3))
+      (should= 2 (euler-2 4))
+      (should= 2 (euler-2 5))
+      (should= 2 (euler-2 6))
+      (should= 2 (euler-2 7))
+      (should= 10 (euler-2 8))
+      (should= 10 (euler-2 9))
+      (should= 10 (euler-2 10))
+      (should= 10 (euler-2 11))
+      (should= 10 (euler-2 13))
+      (should= 44 (euler-2 34))
+      (should= 4613732 (euler-2 4000000))
+      )
   )
 
 (run-specs)
--- src/euler/level1/problem002.clj
+++ src/euler/level1/problem002.clj
@@ -1,4 +1,23 @@
 (ns euler.level1.problem002)
 
-(defn euler-2 [n]
+(defn get-fibonacci-to-n [n]
+  (cond (= n 2) [1 2]
+        (> n 2)
+          (loop [n n
+                 sequence [1 2]]
+            (if (> (reduce + (take-last 2 sequence)) n)
+              sequence
+              (recur n (conj sequence (reduce + (take-last 2 sequence))))
+            )
+
+          )
+        )
   )
+(defn get-sum-of-even [coll]
+  (reduce + (filter even? coll))
+  )
+
+
+(defn euler-2 [n]
+    (get-sum-of-even (get-fibonacci-to-n n))
+    )
