c2af01bb3ae1283ec374e0f4bc5a0c1220958eba
7vanael <kristi.n.norris777@gmail.com>
2025-03-12 14:01:28 -0700

    Polished 10

--- spec/euler/level1/problem010_spec.clj
+++ spec/euler/level1/problem010_spec.clj
@@ -9,16 +9,11 @@
 
 (describe "Euler Problem #10"
 
-  #_(it "finds the index of helper/primes of the highest prime < n"
-    (should= 0 (primes-max-index 3)))
-
   (it "Solves #10"
     (should= 17 (euler-10 10))
     (should= 17 (euler-10 11))
     (should= 28 (euler-10 12))
     (should= 41 (euler-10 14))
-    (should= 142913828922 (euler-10 2000000))
-    )
-  )
+    (should= 142913828922 (euler-10 2000000))))
 
 (run-specs)
--- src/euler/level1/problem010.clj
+++ src/euler/level1/problem010.clj
@@ -1,19 +1,5 @@
 (ns euler.level1.problem010
   (:require [euler.level1.helper :as helper]))
 
-#_(defn primes-max-index [n]
-  (let [primes (nth helper/primes (range))]
-   (when (< n primes)
-     (apply + (rest primes))
-     )))
-
 (defn euler-10 [n]
-  (apply + (filter helper/prime? (range 1 n)))
-  #_(apply + (nth helper/primes (range 0 (inc (primes-index n)))))
-  #_(->> n
-       primes-max-index
-       (range 0 )
-       (nth helper/primes)
-       (apply +))
-  )
-
+  (apply + (take-while #(< % n) helper/primes)))
